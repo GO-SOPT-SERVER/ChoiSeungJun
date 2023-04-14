49150de8e071cc9ad06feaa937218427def42db0
JUN <chltmdwns96@naver.com>
2023-04-14 17:30:05 +0900

    project init

---
+++ firstSeminarAdvancedAssignment/src/main/java/sopt/org/firstSeminarAdvancedAssignment/FirstSeminarAdvancedAssignmentApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.firstSeminarAdvancedAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSeminarAdvancedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSeminarAdvancedAssignmentApplication.class, args);
+	}
+
+}
