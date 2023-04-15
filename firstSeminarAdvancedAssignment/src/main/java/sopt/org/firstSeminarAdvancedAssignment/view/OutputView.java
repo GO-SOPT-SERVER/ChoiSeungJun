a1df571e72a6e209255e966d107703331d23d729
JUN <chltmdwns96@naver.com>
2023-04-15 21:11:56 +0900

    refactor: 메서드 final 키워드 제거 #1

--- firstSeminarAdvancedAssignment/src/main/java/sopt/org/firstSeminarAdvancedAssignment/view/OutputView.java
+++ firstSeminarAdvancedAssignment/src/main/java/sopt/org/firstSeminarAdvancedAssignment/view/OutputView.java
@@ -3,46 +3,46 @@ package sopt.org.firstSeminarAdvancedAssignment.view;
 public class OutputView {
 
     // 고객 등록 메세지
-    public final void printInputClientInfoMessage() {
+    public void printInputClientInfoMessage() {
         System.out.println("고객 등록을 위하여, 정보를 입력해주세요.");
     }
 
-    public final void printInputAccountNumberForRegisterMessage() {
+    public void printInputAccountNumberForRegisterMessage() {
         System.out.println("등록하실 계좌번호를 입력해주세요. (중복불가)");
     }
 
-    public final void printInputBirthForRegisterMessage() {
+    public void printInputBirthForRegisterMessage() {
         System.out.println("생년월일을 입력해주세요. (2000-01-01 형식)");
     }
 
-    public final void printInputTelNumberForRegisterMessage() {
+    public void printInputTelNumberForRegisterMessage() {
         System.out.println("전화번호를 입력해주세요. (010-1234-1234 형식)");
     }
 
-    public final void printInputAddressForRegisterMessage() {
+    public void printInputAddressForRegisterMessage() {
         System.out.println("거주지 주소를 입력해주세요.");
     }
 
-    public final void printInputPasswordForRegisterMessage() {
+    public void printInputPasswordForRegisterMessage() {
         System.out.println("사용하실 비밀번호를 입력해주세요.");
     }
 
 
     //잔액 조회 메세지
-    public final void printCheckAccountBalanceMessage() {
+    public void printCheckAccountBalanceMessage() {
         System.out.println("잔액 조회를 위해 계좌번호와 비밀번호를 입력해주세요.");
     }
 
-    public final void printClientAmountBalance(int amount) {
+    public void printClientAmountBalance(int amount) {
         System.out.println("해당 계좌의 잔액은 " + amount + "원 입니다.");
     }
 
     //출금 메세지
-    public final void printWithdrawAccountBalanceMessage() {
+    public void printWithdrawAccountBalanceMessage() {
         System.out.println("출금을 위해 계좌번호와 비밀번호를 입력해주세요.");
     }
 
-    public final void printWithdrawAmountMessage() {
+    public void printWithdrawAmountMessage() {
         System.out.println("출금하실 금액을 입력해주세요.");
     }
 
@@ -52,7 +52,7 @@ public class OutputView {
     }
 
     //입금 메세지
-    public final void printDepositIntoAccountMessage() {
+    public void printDepositIntoAccountMessage() {
         System.out.println("입금을 위해 계좌번호를 입력해주세요.");
     }
 
@@ -65,15 +65,15 @@ public class OutputView {
     }
 
     //기본 메세지
-    public final void printInputClientNameMessage() {
+    public void printInputClientNameMessage() {
         System.out.println("이름을 입력해주세요.");
     }
 
-    public final void printInputAccountNumberMessage() {
+    public void printInputAccountNumberMessage() {
         System.out.println("계좌번호를 입력해주세요.");
     }
 
-    public final void printInputPasswordMessage() {
+    public void printInputPasswordMessage() {
         System.out.println("비밀번호를 입력해주세요.");
     }
 
