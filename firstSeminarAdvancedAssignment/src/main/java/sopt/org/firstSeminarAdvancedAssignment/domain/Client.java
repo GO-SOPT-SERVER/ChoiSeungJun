a58fbf95a7b22c9512836657fc2a89f92c6b3963
JUN <chltmdwns96@naver.com>
2023-04-18 16:59:57 +0900

    feat: 고객등록 시, 비밀번호 예외처리 사항 구현 #1
    - 비밀번호가 6자가 아니라면, 예외발생
    - 비밀번호가 정수형이 아니라면, 예외발생

--- firstSeminarAdvancedAssignment/src/main/java/sopt/org/firstSeminarAdvancedAssignment/domain/Client.java
+++ firstSeminarAdvancedAssignment/src/main/java/sopt/org/firstSeminarAdvancedAssignment/domain/Client.java
@@ -14,8 +14,9 @@ public class Client {
 
 
     public Client(String accountNumber, String name, String birth, String tel, String address, String password) {
-        validateAccountNumber(accountNumber);
+        validateAccountNumberForRegister(accountNumber);
         this.accountNumber = accountNumber;
+        validatePasswordForRegister(password);
         this.password = password;
         this.name = name;
         this.birth = birth;
@@ -23,6 +24,7 @@ public class Client {
         this.address = address;
         this.amount = 0;
     }
+
 
     public boolean checkPassword(String password) {
         if (this.password.equals(password)) {
@@ -39,7 +41,9 @@ public class Client {
         return amount;
     }
 
-    private void validateAccountNumber(String accountNumber) {
+
+
+    private void validateAccountNumberForRegister(String accountNumber) {
         String[] seperatedAccountNumber = accountNumber.split("-");
 
         validateAccountNumberLength(seperatedAccountNumber);
@@ -61,6 +65,26 @@ public class Client {
             throw new IllegalArgumentException(createErrorMessage(WRONG_ACCOUNT_NUMBER_FORMAT_ERROR_MESSAGE));
         }
     }
+
+    private void validatePasswordForRegister(String password) {
+        validatePasswordNumberFormat(password);
+        validatePasswordNumberLength(password);
+    }
+
+    private void validatePasswordNumberLength(String password) {
+        if(password.length() != 6) {
+            throw new IllegalArgumentException(createErrorMessage("비밀번호는 6자로 구성되어야합니다."));
+        }
+    }
+
+    private void validatePasswordNumberFormat(String password) {
+        try {
+            Integer.parseInt(password);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(createErrorMessage("비밀번호는 정수형으로 구성되어야합니다."));
+        }
+    }
+
 
     public int transfer(int transferAmount) {
         validateAccountBalanceToTransfer(transferAmount);
