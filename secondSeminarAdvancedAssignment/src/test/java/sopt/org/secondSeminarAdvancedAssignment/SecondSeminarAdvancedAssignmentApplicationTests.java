59ebaf19083c79a4500a6c0e22f7ece3ad6b811f
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-04-21 10:45:12 +0900

    심화과제 project init #2

---
+++ secondSeminarAdvancedAssignment/src/test/java/sopt/org/secondSeminarAdvancedAssignment/SecondSeminarAdvancedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.secondSeminarAdvancedAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSeminarAdvancedAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
