3c44e84ca59b805456403fcf12d931f304634804
JUN <chltmdwns96@naver.com>
2023-04-22 17:54:14 +0900

    3차 세미나 #7

---
+++ thirdSeminar/src/main/java/sopt/org/thirdSeminar/infrastructure/UserRepository.java
@@ -0,0 +1,9 @@
+package sopt.org.thirdSeminar.infrastructure;
+
+import org.springframework.data.repository.Repository;
+import sopt.org.thirdSeminar.domain.User;
+
+public interface UserRepository extends Repository<User, Long> {
+
+    void save(User user);
+}
