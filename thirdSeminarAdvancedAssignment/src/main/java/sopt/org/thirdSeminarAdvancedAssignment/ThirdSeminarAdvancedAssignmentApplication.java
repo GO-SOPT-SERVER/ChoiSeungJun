589a72c0ae96cac11307c5cfab22e71089f510b2
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-05-05 23:31:01 +0900

    project init

---
+++ thirdSeminarAdvancedAssignment/src/main/java/sopt/org/thirdSeminarAdvancedAssignment/ThirdSeminarAdvancedAssignmentApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.thirdSeminarAdvancedAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdSeminarAdvancedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdSeminarAdvancedAssignmentApplication.class, args);
+	}
+
+}
---
+++ thirdSeminarAdvancedAssignment/src/test/java/sopt/org/thirdSeminarAdvancedAssignment/ThirdSeminarAdvancedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.thirdSeminarAdvancedAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdSeminarAdvancedAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
