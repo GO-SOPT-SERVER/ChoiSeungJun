280bf10b70a09922704ff80063aa228790142ea9
JUN <chltmdwns96@naver.com>
2023-05-06 15:50:58 +0900

    project init

--- fourthSeminar/src/main/java/sopt/org/thirdSeminar/FourthSeminarApplication.java
+++ fourthSeminar/src/main/java/sopt/org/thirdSeminar/FourthSeminarApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThirdSeminarApplication {
-
+public class FourthSeminarApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ThirdSeminarApplication.class, args);
+		SpringApplication.run(FourthSeminarApplication.class, args);
 	}
 
 }
