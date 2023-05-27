d2ea153bb2048b1b6a83bf06ec49424cfe443721
JUN <chltmdwns96@naver.com>
2023-05-27 16:34:53 +0900

    7차 세미나 - S3 #15

--- seventhSeminar/src/main/java/sopt/org/fourthSeminar/controller/exception/model/BadRequestException.java
+++ seventhSeminar/src/main/java/sopt/org/fourthSeminar/controller/exception/model/BadRequestException.java
@@ -1,2 +1,10 @@
-package sopt.org.fourthSeminar.controller.exception.model;public class BacRequestException {
+package sopt.org.fourthSeminar.controller.exception.model;
+
+import sopt.org.fourthSeminar.controller.exception.Error;
+
+public class BadRequestException extends SoptException{
+
+    public BadRequestException(Error error, String message) {
+        super(error, message);
+    }
 }
--- seventhSeminar/src/main/java/sopt/org/fourthSeminar/infrastructure/ImageRepository.java
+++ seventhSeminar/src/main/java/sopt/org/fourthSeminar/infrastructure/ImageRepository.java
@@ -1,2 +1,10 @@
-package sopt.org.fourthSeminar.infrastructure;public class ImageRepository {
+package sopt.org.fourthSeminar.infrastructure;
+
+
+import org.springframework.data.repository.Repository;
+import sopt.org.fourthSeminar.domain.Image;
+
+public interface ImageRepository extends Repository<Image, Long> {
+    // CREATE
+    void save(Image image);
 }
